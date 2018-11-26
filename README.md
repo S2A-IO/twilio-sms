22939e40d50bf9be1d829e4ef33404ffc61b4043
awais khan <awais.khan@riksof.com>
2018-11-26 15:22:42 +0500

    relasing twillio sms

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # twilio-sms
 Twilio SMS Sender for S2A
+- [Installation](#installation)
+- [Usage Example](#usage-example)
+
+## Installation
+  ```
+npm install git://github.com/S2A-IO/twilio-sms --save
+  ```
+twilio-sms has the peerDependencies of twilio we have to install it.
+```
+npm install twilio@3.18.0 --save
+```
+## Usage Example
+```
+// Please change the parameters with valid values and parameters to run the case
+data.env = {
+  accountSid: 'ACcf19980c1b7d8bbc9816738989fbd1be',
+  authToken: '5bd474895c2019134c8b5ebd532cff82'
+};
+// Please change the parameters with valid values and parameters to run the case
+data.current = {
+  to : '+923315572435',
+  from : '+14103046211',
+  message : 'Hello'
+};
+```
+
+| Field    | Description      | Required       |
+|----------|-------------|----------------|
+| data   | Data passed to this function.  | **YES** |
+| context      | Client context. Unused. | **YES** |
+| callback      | Callback function to pass back the response. | **YES** |
+  ```
+const smsNotification = require('./../server/index');
+
+smsNotification.handler(data, context, function testHandler ( error, res) {
+  if ( error ) {
+    console.log('error',error);
+  } else {
+    console.log('res',res);
+  }
+});
+  ```
---
+++ test/test.js
@@ -0,0 +1,54 @@
+const smsNotification = require('./../server/index');
+var assert = require('assert');
+let correctParams = {};
+let incorrectParams = {};
+let context = '';
+let status = false;
+// Please change the parameters with valid values and parameters to run the test case
+correctParams.env = {
+  accountSid: 'ACcf19980c1b7d8bbc9816738989fbd1be',
+  authToken: '5bd474895c2019134c8b5ebd532cff82'
+};
+// Please change the parameters with valid values and parameters to run the test case
+correctParams.current = {
+  to : '+923315572435',
+  from : '+14103046211',
+  message : 'Hello'
+};
+//incorrect accountSid and incorrect authToken
+incorrectParams.env = {
+  accountSid: 'ACcf19980c1b7d8bbc9816738989fbd1be0',
+  authToken: '5bd474895c2019134c8b5ebd532cff82'
+};
+// Please change the parameters with valid values and parameters to run the test case
+incorrectParams.current = {
+  to : '+923315572435',
+  from : '+14103046211',
+  message : 'Hello'
+};
+
+describe('sms notification with correct params', function () {
+  it('sms notification with correct params', function (done) {
+    this.timeout(15000);
+    setTimeout(done, 15000);
+    smsNotification.handler(correctParams, context, function testHandler ( error, res) {
+      if ( res && res.status && res.status == 'queued') {
+        status = true;
+      }
+      assert.equal(status, true);
+      done();
+    });
+  })
+})
+describe('sms notification with incorrect accountSid and authToken', function () {
+  it('sms notification with incorrect accountSid and authToken', function (done) {
+    smsNotification.handler(incorrectParams, context, function testHandler ( error, res) {
+      let status = false;
+      if ( error && error.status && error.status == 401) {
+        status = true;
+      }
+      assert.equal(status, true);
+      done();
+    });
+  })
+})
